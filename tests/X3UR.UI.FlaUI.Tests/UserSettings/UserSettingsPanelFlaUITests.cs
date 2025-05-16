21cb48a10594f06eda74e110cd57a215e42346de
xMilten <MasterMilten1@gmx.de>
2025-05-16 17:31:23 +0200

    UniverseSettingsTabViewModel Width und Height versucht zu bergenzen

--- tests/X3UR.UI.FlaUI.Tests/UserSettings/UserSettingsPanelFlaUITests.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace X3UR.UI.FlaUI.Tests.UserSettings;
-public class UserSettingsPanelFlaUITests : IDisposable {
-    public void Dispose() {
-        throw new NotImplementedException();
-    }
-}
